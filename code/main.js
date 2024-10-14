402fd8e4e039bff0168bcc4b5aab43f1356e6898
Zer02 <abasdeo29@gmail.com>
2024-10-13 23:58:19 -0400

    commented out starter code and started writing from scratch

--- code/main.js
+++ code/main.js
@@ -1,27 +1,46 @@
 import kaplay from "kaplay";
 
-kaplay();
-loadBean();
-setGravity(1600);
+// kaplay();
+// loadBean();
+// setGravity(1600);
 
-const player = add([
-  sprite("bean"),
-  pos(center()),
-  area(),
-  body(),
+// const player = add([
+//   sprite("bean"),
+//   pos(center()),
+//   area(),
+//   body(),
+// ]);
+
+// add([
+//   rect(width(), 48),
+//   outline(4),
+//   area(),
+//   pos(0, height() - 48),
+//   body({ isStatic: true })
+// ]);
+
+// onKeyPress("space", () => {
+//   if (player.isGrounded()) {
+//     player.jump();
+//   }
+// })
+
+
+const k = kaplay({
+  width: 1280,
+  height: 720
+});
+
+k.loadBean();
+k.setGravity(1000);
+
+const player = k.add([
+  k.sprite("bean"),
+  k.pos(k.center()),
+  k.area(),
+  k.body()
 ]);
 
-add([
-  rect(width(), 48),
-  outline(4),
-  area(),
-  pos(0, height() - 48),
-  body({ isStatic: true })
-]);
-
-onKeyPress("space", () => {
-  if (player.isGrounded()) {
-    player.jump();
-  }
-})
-
+// k.add([
+//   k.rect([k.width(), 300, k.pos(0, 300)])
+// ])
